4331765222a402628297d36653ac937fd34bb9f4
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-12-03 10:24:32 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -9,6 +9,7 @@ public class calculator {
         
         circle   cir=new circle(5.5);
         Rectangle react=new Rectangle(10,5);
+        
         double cirarea=Math.PI *Math.pow(cir.radius,2);
         double reacArea = react.breadth*react.length;
 
